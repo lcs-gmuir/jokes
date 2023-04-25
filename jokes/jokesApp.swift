b850718fa8624e98944046827bb241b8371c5d02
griffin muir <gmuir@lcs.on.ca>
2023-04-25 19:27:42 -0400

    Initial Commit

---
+++ jokes/jokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  jokesApp.swift
+//  jokes
+//
+//  Created by Samantha Stewart on 2023-04-25.
+//
+
+import SwiftUI
+
+@main
+struct jokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
